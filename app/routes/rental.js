3a60416227f03b5242c14f5252755a6987bcd88d
Mara Timberlake <maratimberlake@msn.com>
2017-08-09 08:57:46 -0700

    fix bugs in app/models/review.js and app/routes/rental.js

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -31,15 +31,10 @@ export default Ember.Route.extend({
         return rental.save();
       });
       this.transitionTo('rental', rental);
-    }
-    destroyRental(rental) {
-      var review_deletions = rental.get('reviews').map(function(review) {
-        return review.destroyRecord();
-      });
-      Ember.RSVP.all(review_deletions).then(function() {
-        return rental.destroyRecord();
-      });
-      this.transitionTo('index');
     },
+    destroyReview(review) {
+      review.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
