92f0df7f310850d8a4a59b211b5a6c909e5bacd3
Mara Timberlake <maratimberlake@msn.com>
2017-08-09 08:36:39 -0700

    add deletion button for reviews and rentals

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -32,9 +32,14 @@ export default Ember.Route.extend({
       });
       this.transitionTo('rental', rental);
     }
-    destroyReview(review) {
-      review.destroyRecord();
+    destroyRental(rental) {
+      var review_deletions = rental.get('reviews').map(function(review) {
+        return review.destroyRecord();
+      });
+      Ember.RSVP.all(review_deletions).then(function() {
+        return rental.destroyRecord();
+      });
       this.transitionTo('index');
-    }
+    },
   }
 });
